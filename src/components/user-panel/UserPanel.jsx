e0ac154cebcf185eae9ded71f3393f000061d8d9
dawitmanukian <davit.manukyan.d@tumo.org>
2024-01-10 21:03:14 +0400

    updates

--- src/components/user-panel/UserPanel.jsx
+++ src/components/user-panel/UserPanel.jsx
@@ -5,14 +5,20 @@ const UserPanel = () => {
   return (
     <div className={classes.user_panel}>
       <div className={classes.user_data}>
-        <img
-          src="https://media.istockphoto.com/id/1573329496/photo/multi-layers-color-texture-3d-papercut-layers-in-gradient-vector-banner-carving-art-cover.webp?b=1&s=170667a&w=0&k=20&c=fw4EudwOH6cg8roUz_A4ARgGIlz6na94y6TVUUzYz6k="
-          height={"140px"}
-          width={"140px"}
-        />
-        <p className={classes.user_name}>Davit Manukyan</p>
-        <p className={classes.user_email}>thedavitmanukyan@gmail.com</p>
-        <p className={classes.user_status}>Student</p>
+        <div style={{ height: '200px'}}>
+          <div className={classes.background_color}>
+            <img
+              src="https://media.istockphoto.com/id/1573329496/photo/multi-layers-color-texture-3d-papercut-layers-in-gradient-vector-banner-carving-art-cover.webp?b=1&s=170667a&w=0&k=20&c=fw4EudwOH6cg8roUz_A4ARgGIlz6na94y6TVUUzYz6k="
+              height={"140px"}
+              width={"140px"}
+            />
+          </div>
+        </div>
+        <div>
+          <p className={classes.user_name}>Davit Manukyan</p>
+          <p className={classes.user_email}>thedavitmanukyan@gmail.com</p>
+          <p className={classes.user_status}>Student</p>
+        </div>
       </div>
     </div>
   );
