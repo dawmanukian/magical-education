a298881f0c271ea0b2353ae0ad1d2f7281686d65
dawitmanukian <davit.manukyan.d@tumo.org>
2024-01-10 14:50:31 +0400

    updates

--- src/components/my-tasks/MyTasks.jsx
+++ src/components/my-tasks/MyTasks.jsx
@@ -5,17 +5,29 @@ const MyTasks = () => {
   return (
     <div className={classes.my_tasks}>
       <div className={classes.tasks_panel}>
+          <div className={classes.tasks_div}>
+            <div
+              className={classes.line_div}
+              style={{ background: "#8833FFFF" }}
+            ></div>
+            <p>TO DO</p>
+            <div className={classes.num_div}>0</div>
+          </div>
         <div className={classes.tasks_div}>
-          <div className={classes.line_div} style={{background: '#8833FFFF'}}></div>
-          <p>TO DO</p>
-        </div>
-        <div className={classes.tasks_div}>
-          <div className={classes.line_div} style={{background: '#33BFFFFF'}}></div>
+          <div
+            className={classes.line_div}
+            style={{ background: "#33BFFFFF" }}
+          ></div>
           <p>IN WORK</p>
+          <div className={classes.num_div}>0</div>
         </div>
         <div className={classes.tasks_div}>
-          <div className={classes.line_div} style={{background: 'green'}}></div>
+          <div
+            className={classes.line_div}
+            style={{ background: "green" }}
+          ></div>
           <p>DONE</p>
+          <div className={classes.num_div}>0</div>
         </div>
       </div>
     </div>
